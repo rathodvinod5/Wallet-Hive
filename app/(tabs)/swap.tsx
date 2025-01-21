a7deb7d5d371f6aa8a9f327b214e60e160168203
rathodvinod5 <rathodvinod5@gmail.com>
2025-01-21 20:42:27 +0530

    added button in swap tab contents

--- app/(tabs)/swap.tsx
+++ app/(tabs)/swap.tsx
@@ -1,12 +1,16 @@
-import { SafeAreaView, StyleSheet, View, Text } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import TabContainer from "../modules/swap-module/TabContainer";
 import useSwapContoller from "../modules/swap-module/useSwapController";
 import SingleSwapContainer from "../modules/swap-module/SingleSwapContainer";
+import { Colors } from "@/constants/Colors";
+import { ThemedText } from "@/components/ThemedText";
 
 const SwapScreen = () => {
   const { currentTabIndex, onChangeToken, onChangeTabIndex, fromToken, toToken, toTokenValue,
     fromTokenValue, onChangeFromTokenValue, onChangeToTokenValue } = useSwapContoller();
+
+  const isActive = false;
 
   return(
     <View style={styles.container}>
@@ -17,26 +21,45 @@ const SwapScreen = () => {
          />
         <View style={styles.contentContainer}>
           {currentTabIndex === 0 ? (
-            <View style={styles.swapTopContainer}>
-              <SingleSwapContainer
-                token={fromToken}
-                tokenValue={fromTokenValue}
-                enableTextInput={true}
-                onChangeTextInput={onChangeFromTokenValue}
-              />
-              <SingleSwapContainer
-                token={toToken}
-                tokenValue={toTokenValue}
-                enableTextInput={false}
-                onChangeTextInput={onChangeToTokenValue}
-              />
-              <View style={styles.swapIconFixedContainer}>
-                <MaterialCommunityIcons 
-                  name="swap-vertical-variant" 
-                  size={28} 
-                  color="gray"
+            <View style={{ width: '100%', height: '100%' }}>
+              <View style={styles.swapTopContainer}>
+                <SingleSwapContainer
+                  token={fromToken}
+                  tokenValue={fromTokenValue}
+                  enableTextInput={true}
+                  onChangeTextInput={onChangeFromTokenValue}
                 />
+                <SingleSwapContainer
+                  token={toToken}
+                  tokenValue={toTokenValue}
+                  enableTextInput={false}
+                  onChangeTextInput={onChangeToTokenValue}
+                />
+                <View style={styles.swapIconFixedContainer}>
+                  <MaterialCommunityIcons 
+                    name="swap-vertical-variant" 
+                    size={28} 
+                    color="gray"
+                  />
+                </View>
               </View>
+
+              <TouchableOpacity
+                activeOpacity={0.9}
+                style={[styles.buttonStyles, {
+                  backgroundColor: Colors.light.greyExtraLight1
+                }]}
+                disabled={!isActive}
+              >
+                <ThemedText 
+                  type="textSMSemibold" 
+                  style={[styles.buttonText, {
+                    color: isActive ? "white" : 'gray'
+                  }]}
+                >
+                  Continue
+                </ThemedText>
+              </TouchableOpacity>
             </View>
           ) : (
             <View>
@@ -79,5 +102,17 @@ const styles = StyleSheet.create({
     marginTop: -(HEIGHT / 2),
     borderRadius: WIDTH / 2,
     backgroundColor: 'rgb(242, 242, 242)',
+  },
+  buttonStyles: {
+    width: '100%',
+    height: 48,
+    borderRadius: 48 / 2,
+    // backgroundColor: 'lightgrey'
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40
+  },
+  buttonText: {
   }
 });
