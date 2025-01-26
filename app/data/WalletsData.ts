5078f7f817e530fe94ef8ce60379ffd7aabfa7ab
rathodvinod5 <rathodvinod5@gmail.com>
2025-01-26 22:27:48 +0530

    added code to render coin and nft's from selected wallet

--- app/data/WalletsData.ts
+++ app/data/WalletsData.ts
@@ -1,13 +1,68 @@
+export type CoinType = {
+  chain: {
+    title: string,
+    symbol: string,
+  },
+  cryptoNativeValue: string,
+  quantity: string,
+}
+
+export type NftType = {
+  chain: {
+    title: string,
+    symbol: string,
+  },
+  cryptoNativeValue: string,
+  quantity: string,
+  tokenId: string,
+  metadataUrl: string,
+  nftImageUrl: string,
+};
 
 export type WalletType = {
   walletName: string;
   walletId: string;
   chain: string;
   amount: number;
-  nfts: string[];
+  coins?: CoinType[];
+  nfts?: NftType[];
 }
 
 export const walletsAdded: WalletType[] = [
-  { walletName: 'Ethereum', walletId: '0x1234567890',  chain: 'eth', amount: 20.0, nfts: [] },
-  { walletName: 'Solana', walletId: '0x1234567891',  chain: 'sol', amount: 10.0, nfts: [] },
+  { 
+    walletName: 'Ethereum', 
+    walletId: '0x1234567890',  
+    chain: 'eth', 
+    amount: 20.0, 
+    coins: [
+      { chain: { title: 'Ethereum', symbol: 'ETH' }, cryptoNativeValue: '3431.10', quantity: '0.0001' },
+      { chain: { title: 'Ethereum', symbol: 'ETH' }, cryptoNativeValue: '3431.10', quantity: '0.0001' },
+      { chain: { title: 'Solana', symbol: 'SOL' }, cryptoNativeValue: '150.25', quantity: '0.5' },
+      { chain: { title: 'Binance Smart Chain', symbol: 'BNB' }, cryptoNativeValue: '450.75', quantity: '1.2' }
+    ],
+    nfts: [
+      { chain: { title: 'Ethereum', symbol: 'ETH' }, 
+        cryptoNativeValue: '3431.10', 
+        quantity: '0.0001', 
+        tokenId  : '0x1234567890', 
+        metadataUrl: 'https://www.google.com', 
+        nftImageUrl: 'https://www.google.com' 
+        }, 
+      { chain: { title: 'Solana', symbol: 'SOL' }, 
+        cryptoNativeValue: '150.25', 
+        quantity: '0.5', 
+        tokenId  : '0x1234567890', 
+        metadataUrl: 'https://www.google.com', 
+        nftImageUrl: 'https://www.google.com' 
+        },
+      { chain: { title: 'Binance Smart Chain', symbol: 'BNB' }, 
+        cryptoNativeValue: '450.75', 
+        quantity: '1.2', 
+        tokenId  : '0x1234567890', 
+        metadataUrl: 'https://www.google.com', 
+        nftImageUrl: 'https://www.google.com' 
+      }
+    ]
+  },
+  { walletName: 'Solana', walletId: '0x1234567891',  chain: 'sol', amount: 10.0, coins: [], nfts: [] },
 ];
--- app/modules/explore-tab/CryptoNFTTabContainer.tsx
+++ app/modules/explore-tab/CryptoNFTTabContainer.tsx
@@ -1,12 +1,17 @@
 import { ThemedText } from "@/components/ThemedText";
 import TabButton from "@/components/ui/button/TabButton";
 import useExploreTabController from "@/hooks/useExploreTabController";
-import { View, StyleSheet, TouchableOpacity, FlatList } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import { transactionListData, TransactionObjType } from "@/app/data/DATA";
 import { Link } from "expo-router";
+import { useAppContext } from "@/app/context/ParentContext";
+import { CoinType } from "@/app/data/WalletsData";
 
 const CryptoNFTTabContainer = () => {
   const { activeTab, onChangeTab } = useExploreTabController();
+  const { selectedWallet } = useAppContext();
+  const { coins, nfts } = selectedWallet || { coins: [], nfts: [] };
+
   return(
     <View style={styles.container}>
       <View style={styles.tabContainer}>
@@ -23,31 +28,45 @@ const CryptoNFTTabContainer = () => {
           onPressHandler={onChangeTab}
         />
       </View>
+
+      {coins && coins.length > 0 && activeTab === 0 ? (
+        <View>
+          {coins?.map((coin, index) => <RenderCoinItem key={index} item={coin} />)}
+        </View>
+       ) : nfts && nfts.length > 0 && activeTab === 1 ? (
+        <View>
+          {nfts?.map((coin, index) => <RenderCoinItem key={index} item={coin} />)}
+        </View>
+       ) : (
+        <View style={{ alignItems: 'center', marginTop: 20 }}>
+          <ThemedText type="textMDSemibold">No data</ThemedText>
+        </View>
+      )}
       
-      <FlatList
+      {/* <FlatList
         data={transactionListData}
         keyExtractor={(item, index) => 'list-item-'+index} 
         renderItem={(item) => <RenderItem item={item.item} />}
         contentContainerStyle={styles.flatlistContainer}
-      />
+      /> */}
     </View>
   );
 }
 
 export default CryptoNFTTabContainer;
 
-export const RenderFlatlistForCoinAndNFT = () => {
-  return(
-    <FlatList
-        data={transactionListData}
-        keyExtractor={(item, index) => 'list-item-'+index} 
-        renderItem={(item) => <RenderItem item={item.item} />}
-        contentContainerStyle={styles.flatlistContainer}
-      />
-  );
-}
+// export const RenderFlatlistForCoinAndNFT = () => {
+//   return(
+//     <FlatList
+//       data={transactionListData}
+//       keyExtractor={(item, index) => 'list-item-'+index} 
+//       renderItem={(item) => <RenderItem item={item.item} />}
+//       contentContainerStyle={styles.flatlistContainer}
+//     />
+//   );
+// }
 
-export const RenderItem = ({ item } : { item: TransactionObjType }) => {
+export const RenderCoinItem = ({ item } : { item: CoinType }) => {
   return(
     <View style={{ marginVertical: 8 }}>
       <Link href={"/coindetailsscreen"}>
@@ -57,7 +76,7 @@ export const RenderItem = ({ item } : { item: TransactionObjType }) => {
           <View style={styles.itemRightContainer}>
             <View style={styles.recordContainer}>
               <ThemedText type="textSMSemibold">{item.chain.title}</ThemedText>
-              <ThemedText type="textSMSemibold">{item.transactionAmount}</ThemedText>
+              <ThemedText type="textSMSemibold">{item.quantity}</ThemedText>
             </View>
             <View style={styles.recordContainer}>
               <ThemedText type="textSMSemibold">{item.cryptoNativeValue}</ThemedText>
